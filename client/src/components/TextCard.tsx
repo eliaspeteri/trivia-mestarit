c0daf2eb065c0e02ec2ff9fafea031b6e74a0199
Jani Sillanpaa <jani.k.sillanpaa@tuni.fi>
2021-09-17 16:18:25 +0300

    highlight selected answer card and get actual answer

--- client/src/components/TextCard.tsx
+++ client/src/components/TextCard.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Dispatch, SetStateAction } from 'react';
+import CSS from 'csstype';
 
 /** UI, Css */
 import { Card } from 'semantic-ui-react';
@@ -6,15 +8,29 @@ import '../styles/TextCard.css';
 
 interface Props {
   className?: string;
+  selectedAnswer?: string;
+  setSelectedAnswer?: Dispatch<SetStateAction<string>>;
   text: string;
 }
 
+const selectedStyles: CSS.Properties = {
+  backgroundColor: 'orange !important',
+  border: 'solid 2px orange'
+};
+
 const TextCard: React.FC<Props> = ({
   className = 'text-card',
+  selectedAnswer,
+  setSelectedAnswer,
   text
 }: Props) => {
   return (
-    <Card fluid className={className}>
+    <Card
+      className={className}
+      fluid
+      style={text === selectedAnswer ? selectedStyles : {}}
+      onClick={() => setSelectedAnswer && setSelectedAnswer(text)}
+    >
       <Card.Content>
         <p className="card-text">{text}</p>
       </Card.Content>

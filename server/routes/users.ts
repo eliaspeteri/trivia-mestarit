beeb40811928d12c776539c60524825c0c87600b
Elias Peteri <elias.peteri@hotmail.com>
2021-09-23 17:15:33 +0300

    updated typing and removed console.logs

--- server/routes/users.ts
+++ server/routes/users.ts
@@ -1,40 +1,42 @@
 import { Router, Request, Response } from 'express';
 import bcrypt from 'bcrypt';
-
+import logger from '../utils/logger';
 import User from '../models/user';
 
 const router: Router = Router();
 
 // Get all users
 router.get('/', async (_req: Request, res: Response): Promise<void> => {
-  const users = await User.find({});
-  console.log('fetched all users!');
+  const users: any = (await User.find({})) as any;
+  logger.info('fetched all users!');
   res.json(users);
 });
 
 // Get user by ID
 router.get('/:id', async (req: Request, res: Response): Promise<void> => {
-  const user = User.findById(req.params.id);
+  const user: any = (await User.findById(req.params.id)) as any;
   user ? res.json(user) : res.sendStatus(404);
+  logger.info(`fetched ${req.params.id}`);
 });
 
 // Add new user
 router.post('/', async (req: Request, res: Response): Promise<void> => {
-  const body = req.body;
+  const body: any = req.body as any;
   const saltRounds = 10;
   const passwordHash: string = await bcrypt.hash(body.password, saltRounds);
   try {
-    console.log(body);
+    logger.info(body);
     const user = new User({
       username: body.username,
       passwordHash
     });
 
-    const savedUser = await user.save();
+    const savedUser: any = (await user.save()) as any;
 
     res.json(savedUser.toJSON());
-  } catch (e) {
+  } catch (error) {
     res.status(400).send({ error: 'Unable to post a new user.' });
+    logger.error((error as any).message);
   }
 });
 
@@ -43,8 +45,10 @@ router.delete('/:id', async (req: Request, res: Response): Promise<void> => {
   try {
     await User.findByIdAndRemove(req.params.id);
     res.status(204);
-  } catch (e) {
+    logger.info(`removed ${req.params.id}`);
+  } catch (error) {
     res.json({ error: 'User not found' });
+    logger.error((error as any).message);
   }
 });
 

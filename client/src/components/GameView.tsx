69cd5247c7dd3a3d31b347f2d3f6af238e70c275
Roope Sillanpaa <roope.sillanpaa@tuni.fi>
2021-09-23 11:05:09 +0300

    Merge fix osa 1

--- client/src/components/GameView.tsx
+++ client/src/components/GameView.tsx
@@ -1,40 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 /** Components */
 import ProgressBar from './ProgressBar';
-import QuestionCard from './QuestionCard';
+import TextCard from './TextCard';
 
 /** UI, Css */
-import { Button } from 'semantic-ui-react';
-import '../styles/GameView.css';
+import { Container, Grid, GridRow } from 'semantic-ui-react';
 
 const GameView: React.FC = () => {
-  const procent = 22;
-  const question = '1 + 1 is three';
+  const [selectedAnswer, setSelectedAnswer] = useState<string>('');
+
+  const percent = 22;
+  const question =
+    'Minkä niminen suuri suunnistuskipailu kilpaillaan Suomessa kesäisin?';
 
   return (
-    <main className="gameview">
-      <QuestionCard className={'question-card'} question={question} />
-      <div className="answer-btn-row">
-        <Button size="massive" className="answer-btn">
-          answer1
-        </Button>
-        <Button size="massive" className="answer-btn">
-          answer2
-        </Button>
-      </div>
-      <div className="answer-btn-row">
-        <Button size="massive" className="answer-btn">
-          answer3
-        </Button>
-        <Button size="massive" className="answer-btn">
-          answer4
-        </Button>
-      </div>
-      <div>
-      <ProgressBar progress={procent} />
-      </div>
-    </main>
+    <Container>
+      {selectedAnswer + ' salainen vastaus'}
+      <Grid centered padded columns={1}>
+        <Grid.Column>
+          <TextCard className={'question-card'} text={question} />
+        </Grid.Column>
+
+        <Grid.Row centered columns={2}>
+          <Grid.Column>
+            <TextCard
+              selectedAnswer={selectedAnswer}
+              setSelectedAnswer={setSelectedAnswer}
+              text={'oh canada'}
+            />
+          </Grid.Column>
+          <Grid.Column>
+            <TextCard
+              selectedAnswer={selectedAnswer}
+              setSelectedAnswer={setSelectedAnswer}
+              text={'en muista en muista en muista'}
+            />
+          </Grid.Column>
+        </Grid.Row>
+
+        <Grid.Row centered columns={2}>
+          <Grid.Column>
+            <TextCard
+              selectedAnswer={selectedAnswer}
+              setSelectedAnswer={setSelectedAnswer}
+              text={'puukko juoksu'}
+            />
+          </Grid.Column>
+          <Grid.Column>
+            <TextCard
+              selectedAnswer={selectedAnswer}
+              setSelectedAnswer={setSelectedAnswer}
+              text={'eri vastaus en muista'}
+            />
+          </Grid.Column>
+        </Grid.Row>
+
+        <GridRow centered columns={1}>
+          <Grid.Column>
+            <ProgressBar progress={percent} />
+          </Grid.Column>
+        </GridRow>
+      </Grid>
+    </Container>
   );
 };
 
--- client/src/components/ProgressBar.tsx
+++ client/src/components/ProgressBar.tsx
@@ -5,7 +5,15 @@ interface Props {
 }
 
 const ProgressBar: React.FC<Props> = ({ progress }: Props) => {
+
+  return (
+    <div>
+      <h1 style={{ color: 'white' }}>
   return <div>Very cool and funny progressbar br... {`${progress}`} %</div>; 
+  </h1>
+</div>
+
+);
 };
 
 export default ProgressBar; 

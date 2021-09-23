0cb9708dd1a58868594e477e0d39659f3e61dfb5
Roope Sillanpaa <roope.sillanpaa@tuni.fi>
2021-09-23 11:11:49 +0300

    Merge fix pt 2

--- client/src/components/ProgressBar.tsx
+++ client/src/components/ProgressBar.tsx
@@ -5,15 +5,13 @@ interface Props {
 }
 
 const ProgressBar: React.FC<Props> = ({ progress }: Props) => {
-
   return (
     <div>
       <h1 style={{ color: 'white' }}>
-  return <div>Very cool and funny progressbar br... {`${progress}`} %</div>; 
-  </h1>
-</div>
-
-);
+        Very cool and funny progressbar br... {`${progress} `} %
+      </h1>
+    </div>
+  );
 };
 
-export default ProgressBar; 
+export default ProgressBar;

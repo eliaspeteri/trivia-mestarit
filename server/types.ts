63b6650e0a536ff6e64ed707512416f5f2160e45
Jantero93 <janzku1993@hotmail.com>
2021-09-28 07:55:03 +0300

    TM-57 fix import error, remove duplicate user&question interface

--- server/types.ts
+++ server/types.ts
@@ -1,32 +1,18 @@
 import { Document } from 'mongoose';
 
-export interface Question {
-  id: string;
-  whoCreated?: string;
-  whenCreated: Date;
-  question: string;
-  correctAnswer: string;
-  theme?: string;
-  difficulty: 'easy' | 'medium' | 'hard';
-  answers: string[];
-}
 export interface IQuestion extends Document {
+  id: string;
   whoCreated: string;
   whenCreated: Date;
   question: string;
   correctAnswer: string;
   theme: string;
-  difficulty: string;
-  answers: [string];
-}
-
-export interface User {
-  id: string;
-  username: string;
-  passwordHash: string;
+  difficulty: 'easy' | 'medium' | 'hard';
+  answers: string[];
 }
 
 export interface IUser extends Document {
+  id: string;
   username: string;
   passwordHash: string;
 }

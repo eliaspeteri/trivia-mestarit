f45c1ea5367a2c27c7a873f8914c694cf70a5e0f
Jantero93 <janzku1993@hotmail.com>
2021-09-28 12:14:42 +0300

    merge conflict 2

--- server/types.ts
+++ server/types.ts
@@ -1,6 +1,5 @@
 export type Difficulty = 'easy' | 'medium' | 'hard';
 
-<<<<<<< HEAD
 export type Question = {
   id: string;
   whoCreated?: string;
@@ -17,21 +16,3 @@ export type User = {
   username: string;
   passwordHash: string;
 };
-=======
-export interface IQuestion extends Document {
-  id: string;
-  whoCreated: string;
-  whenCreated: Date;
-  question: string;
-  correctAnswer: string;
-  theme: string;
-  difficulty: 'easy' | 'medium' | 'hard';
-  answers: string[];
-}
-
-export interface IUser extends Document {
-  id: string;
-  username: string;
-  passwordHash: string;
-}
->>>>>>> 1e9bc9a1b698f37cbab2be600185345ea81f9489

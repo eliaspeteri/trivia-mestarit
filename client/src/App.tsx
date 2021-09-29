13adf125fa89b2fa0454a34ae54be000efc4fe0d
Jantero93 <janzku1993@hotmail.com>
2021-09-29 17:29:41 +0300

    TM-39 css fixes, same theme on main menu & game view

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,6 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 /** Components */
-import GameView from './components/GameView';
 import MainMenu from './components/MainMenu';
 
 /** CSS */
@@ -9,13 +8,9 @@ import './App.css';
 import 'semantic-ui-css/semantic.min.css';
 
 const App: React.FC = () => {
-  const [test, setTest] = useState<boolean>(false);
   return (
     <div id="app">
-      <button onClick={() => setTest(!test)}>
-        change component visibility
-      </button>
-      {test ? <GameView /> : <MainMenu />}
+      <MainMenu />
     </div>
   );
 };

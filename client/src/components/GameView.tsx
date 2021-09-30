54c584d1347733d997d70f12458a4b39729d2be4
Jantero93 <janzku1993@hotmail.com>
2021-09-30 16:43:13 +0300

    exit icon and small confirm window on it

--- client/src/components/GameView.tsx
+++ client/src/components/GameView.tsx
@@ -5,7 +5,7 @@ import ProgressBar from './ProgressBar';
 import TextCard from './TextCard';
 
 /** UI, CSS */
-import { Container, Grid, GridRow } from 'semantic-ui-react';
+import { Container, Grid, Icon } from 'semantic-ui-react';
 import '../styles/GameView.css';
 
 const GameView: React.FC = () => {
@@ -15,51 +15,67 @@ const GameView: React.FC = () => {
   const question =
     'Minkä niminen suuri suunnistuskipailu kilpaillaan Suomessa kesäisin?';
 
+  const handleExitIconClick = (): void => {
+    window.confirm('Do you want to abort game?')
+      ? console.log('yes pls')
+      : console.log('no pls');
+  };
+
   return (
-    <Container>
-      {selectedAnswer + ' salainen vastaus'}
-      <Grid columns={1} className="game-view-content" container>
-        <Grid.Column>
-          <TextCard className={'question-card'} text={question} />
-        </Grid.Column>
+    <React.Fragment>
+      <Icon
+        onClick={handleExitIconClick}
+        bordered
+        className="sign-out-icon"
+        name="sign out"
+        size="huge"
+      />
+      <Container>
+        {selectedAnswer + ' salainen vastaus'}
+        <Grid columns={1} className="game-view-content" container>
+          <Grid.Column>
+            <TextCard className={'question-card'} text={question} />
+          </Grid.Column>
 
-        <Grid.Column stretched columns={1}>
-          <TextCard
-            selectedAnswer={selectedAnswer}
-            setSelectedAnswer={setSelectedAnswer}
-            text={'oh  canada'}
-          />
-        </Grid.Column>
-        <Grid.Column stretched columns={1}>
-          <TextCard
-            selectedAnswer={selectedAnswer}
-            setSelectedAnswer={setSelectedAnswer}
-            text={'en muista en muista en muista'}
-          />
-        </Grid.Column>
+          <Grid.Column stretched columns={1}>
+            <TextCard
+              selectedAnswer={selectedAnswer}
+              setSelectedAnswer={setSelectedAnswer}
+              text={'oh canada'}
+            />
+          </Grid.Column>
 
-        <Grid.Column>
-          <TextCard
-            selectedAnswer={selectedAnswer}
-            setSelectedAnswer={setSelectedAnswer}
-            text={
-              'puukko  dsadasd dsdasdasdsasds asdas ds asd asdasd asd juoksu'
-            }
-          />
-        </Grid.Column>
-        <Grid.Column>
-          <TextCard
-            selectedAnswer={selectedAnswer}
-            setSelectedAnswer={setSelectedAnswer}
-            text={'eri vastaus en muista'}
-          />
-        </Grid.Column>
+          <Grid.Column stretched columns={1}>
+            <TextCard
+              selectedAnswer={selectedAnswer}
+              setSelectedAnswer={setSelectedAnswer}
+              text={'en muista en muista en muista'}
+            />
+          </Grid.Column>
 
-        <Grid.Column columns={1}>
+          <Grid.Column>
+            <TextCard
+              selectedAnswer={selectedAnswer}
+              setSelectedAnswer={setSelectedAnswer}
+              text={
+                'puukko  dsadasd dsdasdasdsasds asdas ds asd asdasd asd juoksu'
+              }
+            />
+          </Grid.Column>
+
+          <Grid.Column>
+            <TextCard
+              selectedAnswer={selectedAnswer}
+              setSelectedAnswer={setSelectedAnswer}
+              text={'eri vastaus en muista'}
+            />
+          </Grid.Column>
+
+          <Grid.Column columns={1}></Grid.Column>
           <ProgressBar progress={percent} />
-        </Grid.Column>
-      </Grid>
-    </Container>
+        </Grid>
+      </Container>
+    </React.Fragment>
   );
 };
 

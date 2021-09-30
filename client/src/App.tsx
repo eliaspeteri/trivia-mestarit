0901f4c76eaaf270cc7a2d110d8385dce132e8e1
Jantero93 <janzku1993@hotmail.com>
2021-09-30 16:43:13 +0300

    exit icon and small confirm window on it

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,11 +6,12 @@ import MainMenu from './components/MainMenu';
 /** CSS */
 import './App.css';
 import 'semantic-ui-css/semantic.min.css';
+import GameView from './components/GameView';
 
 const App: React.FC = () => {
   return (
     <div id="app">
-      <MainMenu />
+      <GameView />
     </div>
   );
 };

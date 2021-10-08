63850e59f96d074d1cbbc02f4e7ebd53d9795b62
Jantero93 <janzku1993@hotmail.com>
2021-10-08 18:46:26 +0300

    typo fixes, removed unnecessary imports

--- server/websockets/listeners.ts
+++ server/websockets/listeners.ts
@@ -2,10 +2,8 @@ import { Socket } from 'socket.io';
 import { Server as SocketServer } from 'socket.io';
 import { v4 as uuidv4 } from 'uuid';
 
-export const setListeners = async (io: SocketServer): Promise<void> => {
-  await io.on('connection', (socket: Socket) => {
-    console.log(`client joined: ${socket.id}`);
-
+export const setListeners = (io: SocketServer): void => {
+  io.on('connection', (socket: Socket) => {
     socket.on('host-game', (callback: CallableFunction) => {
       const roomId: string = uuidv4().toString();
       socket.join(roomId);

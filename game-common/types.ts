2ddfcdf49dff78527411fac8c91b9cee45051966
Elias Peteri <elias.peteri@hotmail.com>
2021-10-19 13:37:10 +0300

    Moved common types and configuration variables to a separate npm package

---
+++ game-common/types.ts
@@ -0,0 +1,21 @@
+/* Server game-logic types */
+export type Player = {
+  nick: string;
+  points: number;
+  selectedAnswer: string;
+};
+
+export type Question = {
+  question: string;
+  answers: string[];
+  correctAnswer: string;
+};
+
+export type GameData = {
+  hostNick: string;
+  players: Player[];
+  currentQuestion: Question;
+  questionsTotal: number;
+  currentQuestionIndex: number;
+  showCorrectAnswer: boolean;
+};

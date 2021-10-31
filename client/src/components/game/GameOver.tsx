d82e8fc64744a3e72560085ce076d0673642f498
Jantero93 <janzku1993@hotmail.com>
2021-10-31 09:48:09 +0200

    TM-91 improved end score with sorting

--- client/src/components/game/GameOver.tsx
+++ client/src/components/game/GameOver.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 /** CSS, UI */
-import { Container, Table } from 'semantic-ui-react';
+import { Container, Icon, Table } from 'semantic-ui-react';
 
 /** Types */
 import { Player } from 'game-common';
@@ -11,24 +11,50 @@ interface Props {
 }
 
 const GameOver: React.FC<Props> = ({ players }: Props) => {
-  const sortPlayersByScore = (): Player[] => {
-    return [...players].sort((a, b) =>
-      a.points > b.points ? -1 : b.points > a.points ? 1 : 0
-    );
+  const correspondingTrophyIcon = (index: number): JSX.Element | null => {
+    const trophyIcon = 'trophy';
+
+    switch (index) {
+      case 0:
+        return <Icon color={'yellow'} name={trophyIcon} />;
+
+      case 1:
+        return <Icon color={'grey'} name={trophyIcon} />;
+
+      case 2:
+        return <Icon color={'brown'} name={trophyIcon} />;
+
+      default:
+        return null;
+    }
   };
 
   const mapPlayerScores = (): JSX.Element[] =>
     sortPlayersByScore().map((player: Player, index: number) => (
-      <Table.Row key={index}>
-        <Table.Cell>{index + 1}</Table.Cell>
+      <Table.Row key={index} style={{ backgroundColor: 'black' }}>
+        <Table.Cell>
+          {`${index + 1}.`} {correspondingTrophyIcon(index)}
+        </Table.Cell>
         <Table.Cell>{player.nick}</Table.Cell>
         <Table.Cell>{player.points}</Table.Cell>
       </Table.Row>
     ));
 
+  const sortPlayersByScore = (): Player[] => {
+    /** Descending order */
+    return [...players].sort((a, b) =>
+      a.points > b.points ? -1 : b.points > a.points ? 1 : 0
+    );
+  };
+
   return (
     <Container>
-      <Table inverted padded size={'large'}>
+      <Table
+        inverted
+        padded
+        size={'large'}
+        style={{ border: '1px solid whitesmoke' }}
+      >
         <Table.Header style={{ backgroundColor: '#f2711c' }}>
           <Table.Row>
             <Table.HeaderCell>Position</Table.HeaderCell>
--- server/game-logic/mockupData.ts
+++ server/game-logic/mockupData.ts
@@ -4,5 +4,15 @@ export const mockUpQuestions: Question[] = [
     answers: ['Känädä', 'Suomi', 'Viro', 'Afrikka'],
     correctAnswer: 'Suomi',
     question: 'Missä sijaitsee Tampere?'
+  },
+  {
+    answers: ['1', '1/2', '1002', '4'],
+    correctAnswer: '4',
+    question: '2 + 2 = ?'
+  },
+  {
+    answers: ['Tammikuu', 'Kesäkuu', 'Joulukuu', 'Eiku maassa XD'],
+    correctAnswer: 'Eiku maassa XD',
+    question: 'Missä kuussa vietetään joulua?'
   }
 ];

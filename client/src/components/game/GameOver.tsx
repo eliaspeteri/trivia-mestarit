d8862338e114483dff0eb9bca46e66232abc6b47
Jantero93 <janzku1993@hotmail.com>
2021-10-31 09:14:51 +0200

    table version of end score

--- client/src/components/game/GameOver.tsx
+++ client/src/components/game/GameOver.tsx
@@ -1,49 +1,44 @@
 import React from 'react';
 
 /** CSS, UI */
-import { Container, Grid, List } from 'semantic-ui-react';
+import { Container, Table } from 'semantic-ui-react';
 
 /** Types */
 import { Player } from 'game-common';
-import CSS from 'csstype';
 
 interface Props {
   players: Player[];
 }
 
-const containerStyles: CSS.Properties = {
-  backgroundColor: 'black',
-  position: 'relative',
-  border: '3px solid white',
-  top: '5em',
-  margin: '5em'
-};
-
 const GameOver: React.FC<Props> = ({ players }: Props) => {
+  const sortPlayersByScore = (): Player[] => {
+    return [...players].sort((a, b) =>
+      a.points > b.points ? -1 : b.points > a.points ? 1 : 0
+    );
+  };
+
+  const mapPlayerScores = (): JSX.Element[] =>
+    sortPlayersByScore().map((player: Player, index: number) => (
+      <Table.Row key={index}>
+        <Table.Cell>{index + 1}</Table.Cell>
+        <Table.Cell>{player.nick}</Table.Cell>
+        <Table.Cell>{player.points}</Table.Cell>
+      </Table.Row>
+    ));
+
   return (
     <Container>
-      <h1 style={{ color: 'white' }}>Players and their score:</h1>
+      <Table inverted padded size={'large'}>
+        <Table.Header style={{ backgroundColor: '#f2711c' }}>
+          <Table.Row>
+            <Table.HeaderCell>Position</Table.HeaderCell>
+            <Table.HeaderCell>Player</Table.HeaderCell>
+            <Table.HeaderCell>Points</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
 
-      <Grid container style={containerStyles}>
-        <List
-          divided
-          inverted
-          ordered
-          size={'huge'}
-          style={{ padding: '1em', marginLeft: '1em' }}
-        >
-          {players.map((player: Player, index: number) => (
-            <List.Item
-              key={index}
-              value={player.points.toString()}
-              style={{ color: '#f2711c' }}
-            >
-              <List.Header>{player.nick}</List.Header>
-              {player.points}
-            </List.Item>
-          ))}
-        </List>
-      </Grid>
+        <Table.Body>{mapPlayerScores()}</Table.Body>
+      </Table>
     </Container>
   );
 };

209eadca3cc56f660cfea3361291e5764c872508
Jantero93 <janzku1993@hotmail.com>
2021-11-03 15:54:16 +0200

    TM-91 form for new questions

--- client/src/components/QuestionForm.tsx
+++ client/src/components/QuestionForm.tsx
@@ -1,18 +1,45 @@
 import React, { useState } from 'react';
-import { Container, Form, FormRadioProps, Segment } from 'semantic-ui-react';
+
+/** CSS, UI */
+import { Button, Container, Form, Segment } from 'semantic-ui-react';
 
 const QuestionForm: React.FC = () => {
   const [answer_1, setAnswer_1] = useState<string>('');
   const [answer_2, setAnswer_2] = useState<string>('');
   const [answer_3, setAnswer_3] = useState<string>('');
   const [answer_4, setAnswer_4] = useState<string>('');
+  const [difficulty, setDifficult] = useState<string>('');
   const [correctAnswer, setCorrectAnswer] = useState<string>('');
   const [question, setQuestions] = useState<string>('');
 
+  const answerOptions = [
+    { key: '1', text: 'Answer 1', value: answer_1 },
+    { key: '2', text: 'Answer 2', value: answer_2 },
+    { key: '3', text: 'Answer 3', value: answer_3 },
+    { key: '4', text: 'Answer 4', value: answer_4 }
+  ];
+
+  const difficultyOptions = [
+    { key: '1', text: 'Easy', value: 'easy' },
+    { key: '2', text: 'Medium', value: 'medium' },
+    { key: '3', text: 'Hard', value: 'hard' }
+  ];
+
   const answersAreValid = (): boolean =>
-    [answer_1, answer_2, answer_3, answer_4].every(
+    Array.of(answer_1, answer_2, answer_3, answer_4).every(
       (answer: string) => answer.length
     );
+
+  const submitClicked = (e: any): void => {
+    e.preventDefault();
+    console.log(`answer_1`, answer_1);
+    console.log(`answer_2`, answer_2);
+    console.log(`answer_3`, answer_3);
+    console.log(`answer_4`, answer_4);
+    console.log(`correctAnswer`, correctAnswer);
+    console.log(`difficulty`, difficulty);
+    console.log(`correctAnswer`, correctAnswer);
+  };
 
   return (
     <Container
@@ -24,7 +51,7 @@ const QuestionForm: React.FC = () => {
       }}
     >
       <Segment inverted style={{ paddingTop: '2em' }}>
-        <Form inverted size={'massive'}>
+        <Form inverted size={'huge'}>
           <Form.Field
             control={'textarea'}
             label={'Question'}
@@ -34,7 +61,7 @@ const QuestionForm: React.FC = () => {
             }
           />
 
-          <Form.Group widths="equal">
+          <Form.Group widths={'equal'}>
             <Form.Field
               control={'input'}
               label={'Answer 1'}
@@ -58,7 +85,7 @@ const QuestionForm: React.FC = () => {
           <Form.Group widths={'equal'}>
             <Form.Field
               control={'input'}
-              label={'Answer 4'}
+              label={'Answer 3'}
               rows={'2'}
               value={answer_3}
               onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
@@ -76,42 +103,31 @@ const QuestionForm: React.FC = () => {
             />
           </Form.Group>
 
-          <Form.Group inline as={'button-group'}>
-            <label style={{ color: '#f2711c', padding: '0 1em 0 .25em' }}>
-              Correct Answer
-            </label>
-            <Form.Field
-              label={'1'}
-              control={'input'}
-              type={'radio'}
-              onSelect={() => setCorrectAnswer(answer_1)}
+          <Form.Group style={{ paddingTop: '1em' }} widths={'equal'}>
+            <Form.Select
+              rows={'2'}
+              label="Pick answer"
+              options={answerOptions}
+              onChange={(e, { value }) => setCorrectAnswer(value as string)}
             />
-            <Form.Field
-              label={'2'}
-              control="input"
-              type="radio"
-              onSelect={() => setCorrectAnswer(answer_2)}
-            />
-            <Form.Field
-              label={'3'}
-              control={'input'}
-              type="radio"
-              onSelect={() => setCorrectAnswer(answer_3)}
-            />
-            <Form.Field
-              label={'4'}
-              control={'input'}
-              type={'radio'}
-              onSelect={() => setCorrectAnswer(answer_4)}
+            <Form.Select
+              rows={'2'}
+              label="Pick difficulty"
+              options={difficultyOptions}
+              onChange={(e, { value }) => setDifficult(value as string)}
             />
           </Form.Group>
 
-          <Form.Field
-            control={'button'}
-            disabled={!answersAreValid() || !question}
+          <Button
+            disabled={
+              !answersAreValid() || !difficulty || !question || !correctAnswer
+            }
+            type={'submit'}
+            style={{ margin: '1em' }}
+            onClick={submitClicked}
           >
             Submit
-          </Form.Field>
+          </Button>
         </Form>
       </Segment>
     </Container>

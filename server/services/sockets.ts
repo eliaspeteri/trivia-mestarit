3da84c60594c48a856a194e5844bd0c4c1976757
Jantero93 <janzku1993@hotmail.com>
2021-11-04 18:26:43 +0200

    wip before develop pics

--- server/services/sockets.ts
+++ server/services/sockets.ts
@@ -1,10 +1,66 @@
-/** Types */
-import { Game } from 'game-common';
+/** Class, Types */
+import Game from '../game-logic/Game';
+import { Question } from 'game-common';
 
-const addGame = (): void => console.log('jotain');
+/** ID generator */
+
+/** Service */
+import QuestionService from './questions';
+
+let games: Game[] = [];
+
+const addGame = async (
+  questionsCount: number,
+  roomId: string
+): Promise<void> => {
+  const gameQuestions: Question[] = await QuestionService.getRandomQuestions(
+    questionsCount
+  );
+
+  const newGame = new Game(roomId, gameQuestions);
+  games = games.concat(...games, newGame);
+
+  console.log(`games`, games);
+};
+
+const findGameById = (roomId: string): Game =>
+  games.find((game: Game) => game.roomId === roomId) as Game;
+
+const gameIdExists = (roomId: string): boolean =>
+  games.map((game: Game) => game.roomId).includes(roomId);
+
+const getAllGames = (): Game[] => games;
+
+const joinGame = (nick: string, roomId: string, isHost: boolean): boolean => {
+  console.log(`games`, games);
+  if (!gameIdExists(roomId)) return false;
+
+  const game: Game = findGameById(roomId);
+  game.addPlayer(nick, isHost);
+  return true;
+};
+
+const setAnswer = (answer: string, roomId: string, nick: string): void => {
+  const game: Game = findGameById(roomId);
+  game.setPlayerAnswer(nick, answer);
+  games = updateGame(game);
+};
+
+const removeGame = (gameId: string): Game[] =>
+  games.filter((game: Game) => game.roomId !== gameId);
+
+const updateGame = (updatedGame: Game): Game[] =>
+  games.map((game: Game) =>
+    updatedGame.roomId === game.roomId ? updatedGame : game
+  );
 
 const SocketService = {
-  addGame
+  addGame,
+  findGameById,
+  getAllGames,
+  joinGame,
+  removeGame,
+  setAnswer
 };
 
 export default SocketService;

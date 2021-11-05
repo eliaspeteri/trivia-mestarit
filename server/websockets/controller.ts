24a0ac3027b7dcbf59457c78a81992336145ce8e
Jantero93 <janzku1993@hotmail.com>
2021-11-05 15:56:32 +0200

    TM-120 seperate service and controller on websockets

--- server/websockets/controller.ts
+++ server/websockets/controller.ts
@@ -4,33 +4,21 @@ import { Socket } from 'socket.io';
 
 /** Components */
 import Game from '../game-logic/Game';
-
-/** Utils */
-import {
-  addGame,
-  findGameByRoomId,
-  gameIdExists,
-  updateGame,
-  removeGame
-} from './utils';
 import logger from '../utils/logger';
-import { v4 as uuidv4 } from 'uuid';
 
 /** QuestionService */
-import QuestionService from '../services/questions';
+import SocketService from '../services/sockets';
 
 /** Game config, types */
-import { GameData, TOTAL_TIME_PER_QUESTION, Question } from 'game-common';
+import { GameData } from 'game-common';
 
 export const setListeners = (io: SocketServer): void => {
-  let games: Game[] = [];
-
   /** Send every game's game data */
   setInterval(() => {
-    games.forEach((game: Game) => {
+    SocketService.getGames().forEach((game: Game) => {
       if (game.isLastQuestion()) {
         io.to(game.roomId).emit('game-over', game.getGameData());
-        games = removeGame(games, game.roomId);
+        SocketService.removeGame(game.roomId);
         return;
       }
 
@@ -46,12 +34,8 @@ export const setListeners = (io: SocketServer): void => {
     logger.info(`Socket ID connected: ${socket.id}`);
 
     socket.on('host-game', async (callback: CallableFunction) => {
-      const roomId: string = uuidv4().toString();
-      const gameQuestions: Question[] =
-        await QuestionService.getRandomQuestions(3);
+      const roomId: string = await SocketService.createGame();
 
-      games = addGame(games, new Game(roomId, gameQuestions));
-      /** Returns room ID to client */
       callback({
         gameId: roomId
       });
@@ -60,30 +44,20 @@ export const setListeners = (io: SocketServer): void => {
     socket.on(
       'join-game',
       (nick: string, roomId: string, isHost, callback: CallableFunction) => {
-        if (!gameIdExists(games, roomId)) {
-          callback({ error: true, message: 'No Game with ID' });
-          return;
-        }
-
-        const game: Game = findGameByRoomId(games, roomId);
-        socket.join(game.roomId);
-        game.addPlayer(nick, isHost);
+        SocketService.joinGame(nick, roomId, isHost)
+          ? socket.join(roomId)
+          : callback({ error: true, message: 'No Game with ID' });
       }
     );
 
     socket.on('start-game', (gameId: string) => {
-      gameIdExists(games, gameId) &&
-        findGameByRoomId(games, gameId).startGame(TOTAL_TIME_PER_QUESTION);
+      SocketService.startGame(gameId);
     });
 
     socket.on(
       'selected-answer',
       (answer: string, roomId: string, nick: string) => {
-        if (!gameIdExists(games, roomId)) return;
-
-        const game = findGameByRoomId(games, roomId);
-        game.setPlayerAnswer(nick, answer);
-        games = updateGame(games, game);
+        SocketService.setAnswer(answer, roomId, nick);
       }
     );
   });

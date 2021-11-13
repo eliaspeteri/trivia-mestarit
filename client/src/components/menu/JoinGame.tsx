4c8771022de0b76c56c6be63d57006acad2e5e3e
Jantero93 <janzku1993@hotmail.com>
2021-11-13 10:01:21 +0200

    seperate main menu components, new interaction on menu, css tuning

---
+++ client/src/components/menu/JoinGame.tsx
@@ -0,0 +1,67 @@
+import React, { Dispatch, SetStateAction } from 'react';
+import { useHistory } from 'react-router-dom';
+
+/** CSS, UI */
+import { Button, Divider, Input } from 'semantic-ui-react';
+
+interface Props {
+  gameId: string;
+  nick: string;
+  setGameId: Dispatch<SetStateAction<string>>;
+  setIsHost: Dispatch<SetStateAction<boolean>>;
+  setNick: Dispatch<SetStateAction<string>>;
+}
+
+const JoinGame: React.FC<Props> = ({
+  gameId,
+  nick,
+  setGameId,
+  setIsHost,
+  setNick
+}: Props) => {
+  const history = useHistory();
+
+  const joinHostedGame = (): void => {
+    history.push('/game');
+    setIsHost(false);
+    setNick(nick);
+  };
+
+  return (
+    <>
+      <Divider />
+
+      <Input
+        focus
+        placeholder="Username"
+        value={nick}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+          setNick(e.target.value)
+        }
+      />
+
+      <Divider />
+
+      <Input
+        focus
+        placeholder={'Game ID'}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+          setGameId(e.target.value)
+        }
+      />
+
+      <Divider />
+
+      <Button
+        className="button"
+        color={'orange'}
+        content={'JOIN'}
+        disabled={!gameId && !nick}
+        size={'massive'}
+        onClick={joinHostedGame}
+      />
+    </>
+  );
+};
+
+export default JoinGame;

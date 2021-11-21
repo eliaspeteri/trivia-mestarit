26d1e488c91dfb16b730af5161c460e42b828500
Elias Peteri <elias.peteri@hotmail.com>
2021-11-21 19:38:11 +0200

    Check node environment in case server runs in test mode.

--- server/utils/config.ts
+++ server/utils/config.ts
@@ -4,5 +4,8 @@ dotenv.config();
 
 export default {
   PORT: (process.env.PORT as string) || '8080',
-  MONGODB_URI: process.env.MONGODB_URI as string
+  MONGODB_URI:
+    process.env.NODE_ENV == 'test'
+      ? (process.env.MONGODB_TEST_URI as string)
+      : (process.env.MONGODB_URI as string)
 };

1e931fa463564300c4f66010520ceb1ac44715fb
Jani Sillanpää <jani.k.sillanpaa@tuni.fi>
2021-12-08 12:04:24 +0000

    TM-137 shell scriptit -> npm scripts generoitu koodi pois versiohallinnasta
    Approved-by: Elias Peteri

--- game-common/dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-export { Player, Question, GameData, Difficulty } from './types';
-export { TOTAL_TIME_PER_QUESTION, TIME_TO_ANSWER_QUESTION } from './gameConfig';
--- game-common/dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TIME_TO_ANSWER_QUESTION = exports.TOTAL_TIME_PER_QUESTION = void 0;
-var gameConfig_1 = require("./gameConfig");
-Object.defineProperty(exports, "TOTAL_TIME_PER_QUESTION", { enumerable: true, get: function () { return gameConfig_1.TOTAL_TIME_PER_QUESTION; } });
-Object.defineProperty(exports, "TIME_TO_ANSWER_QUESTION", { enumerable: true, get: function () { return gameConfig_1.TIME_TO_ANSWER_QUESTION; } });
